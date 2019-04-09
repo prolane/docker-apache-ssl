57ea865d5a3f7a1da231279684a5781fa9542685
Gerard Laan <prolane.org@gmail.com>
2019-04-09 22:57:36 +0200

    Fully working version 1.0

--- Dockerfile
+++ Dockerfile
@@ -2,21 +2,28 @@
 FROM ubuntu:18.04
 
 # Update package manager
-RUN apt update
+RUN apt-get update
 # Set timezone info (for non interactive installation of tzdata)
 RUN printf 'tzdata tzdata/Areas select Europe\ntzdata tzdata/Zones/Europe select Amsterdam\n' | debconf-set-selections
 # Needed for non interactive installation of tzdata
 ARG DEBIAN_FRONTEND=noninteractive
 ARG DEBCONF_NONINTERACTIVE_SEEN=true
 # Install default packages
-RUN apt install -y apache2 software-properties-common tzdata
+RUN apt-get install -y apache2 software-properties-common tzdata
+# Enable apache2 proxy support & rewrite mod
+RUN a2enmod proxy && a2enmod proxy_http && a2enmod rewrite && a2enmod headers
 # Add additional repos for installing certbot
 RUN add-apt-repository -y universe && add-apt-repository -y ppa:certbot/certbot && apt-get update
 # Install certbot including apache plugin
-RUN apt install -y certbot python-certbot-apache
+RUN apt-get install -y certbot python-certbot-apache
 
-# Copy in the apache2 config
+# Copy in the apache2 files
 COPY files/apache.conf /etc/apache2/sites-available/000-default.conf
+COPY files/ports.conf /etc/apache2/ports.conf
+COPY files/index.html /var/www/html/index.html
+
+# Have another directory in the apache conf dir to place 'include' files
+RUN mkdir /etc/apache2/conf-include
 
 # Copy in the script that issues a certificate using certbot and starts Apache2 in foreground
 COPY startup.sh /var/run/docker-startup.sh
--- startup.sh
+++ startup.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
-# Check if domain is set
-# If set, set ServerName in the apache.conf
-if [[ -z "${LE_DOMAIN}" ]]; then
-  echo "ServerName is not set in apache conf."
+# If PROXY_HOST and PROXY_PORT env vars are set, 
+# then make sure proxy.conf file is created
+if [[ -z "${PROXY_HOST}" ]] ||  [[ -z "${PROXY_PORT}" ]]; then
+  echo "Apache will not run as ReverseProxy."
+  # Set the 'Proxy' command line parameter for apachectl
+  APACHE_PROXY=NoProxy
 else
-  sed -i '/ServerName/c\ServerName '"$LE_DOMAIN" /etc/apache2/sites-available/000-default.conf
+  echo -e "<Location \"/\">\n\
+  RequestHeader set X-Forwarded-Proto "https"
+  ProxyPass http://$PROXY_HOST:$PROXY_PORT/\n\
+  ProxyPassReverse http://$PROXY_HOST:$PROXY_PORT/\n\
+  </Location>" > /etc/apache2/conf-include/proxy.conf
+  # Set the 'Proxy' command line parameter for apachectl
+  APACHE_PROXY=Proxy
 fi
 
-# Check if required env vars for certbot are both set
-# If both set, run certbot
+# Check if required env vars for certbot are set
 if [[ -z "${LE_EMAIL}" ]] ||  [[ -z "${LE_DOMAIN}" ]]; then
   echo "Apache will not run with ssl/tls endpoint."
 else
+  # Set the right ServerName and use https scheme
+  # Sed explained: Ignore everything from start to first occurrence of 'ServerName'
+  # Then do a regular substitution
+  sed -i "0,/ServerName/! s/.*ServerName.*/\tServerName https:\/\/$LE_DOMAIN/" /etc/apache2/sites-available/000-default.conf
+  # Run certbot
   certbot -n --apache --agree-tos --email ${LE_EMAIL} --domains ${LE_DOMAIN}
   # Start cron (so the certs can be automatically renewed)
   service cron start
@@ -29,4 +41,4 @@ fi
 
 # Start apache in foreground
 echo "Will now start apache2 in foreground."
-apachectl -DFOREGROUND
+apachectl -DFOREGROUND -D$APACHE_PROXY
